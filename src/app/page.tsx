518e39443a9964c4d6ca5e4ae9dd0bf634ffed96
F-kite <ilichevv_v845@mail.ru>
2025-03-25 08:33:21 +0700

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Head from "next/head";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <div className={styles.container}>
+    <>
+      <Head>
+        <title>Black Square</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <div className={styles.page}>
         <div className={styles.blackSquare}></div>
       </div>
-    </div>
+    </>
   );
 }
